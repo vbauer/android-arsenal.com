7d9c912c47b3f667b0e80b29f95973885585fe1c
Vladislav Bauer <bauer.vlad@gmail.com>
2014-09-16 16:14:05 +0700

    Decrease complexity

---
+++ app/base.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'rack/cache'
+require_relative 'model'
+
+class BaseApplication < Sinatra::Base
+    CACHE_TIME = 86400
+
+    configure :production, :development do
+        disable :sessions,  :logging
+
+        set :start_time, Time.now
+        set :data, DataContext.new
+        set :public_folder, File.dirname(__FILE__) + "/public/"
+
+        use Rack::Cache, :verbose => false
+        use Rack::ConditionalGet
+        use Rack::ETag
+        use Rack::Deflater
+    end
+
+    before do
+        start = settings.start_time
+        last_modified start
+        etag start.to_s
+        expires CACHE_TIME
+        cache_control :public, :must_revalidate, :max_age => CACHE_TIME
+    end
+end
