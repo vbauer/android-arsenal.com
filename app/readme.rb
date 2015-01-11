b992c022230f7c849a3ba141208fabdc1c551d4c
Vladislav Bauer <bauer.vlad@gmail.com>
2015-01-11 20:48:10 +0600

    Add TOC in the README.md

--- app/readme.rb
+++ app/readme.rb
@@ -6,16 +6,33 @@ include Sinatra
 include Helpers
 
 
-def render(type, categories, f)
+def render_link(title, url)
+    '[' + title + '](' + url  + ')'
+end
+
+def render_toc(f)
+    f.puts '## Table of content'
+
+    [:free, :demo, :paid].each { |type|
+        category = type_name(type).capitalize
+        anchor = '#' + category.downcase.gsub!(' ', '-')
+        f.puts('* ' + render_link(category, anchor))
+    }
+end
+
+def render_categories(type, data, f)
     f.puts
     f.puts('## ' + type_name(type).capitalize)
 
+    categories = data.categories
+    count = data.count
+
     categories.sort.each_with_index { |(category, projects), index|
-        f.puts('### ' + category)
+        f.puts('### ' + category + ' (' + count.to_s + ')')
         projects.each { |project|
             name = project[ 'name']
             url = project['url']
-            f.puts('* [' + name + '](' + url  + ')')
+            f.puts('* ' + render_link(name, url))
         }
         f.puts
     }
@@ -28,9 +45,10 @@ data = DataContext.new
 FileUtils.cp(header, readme)
 open(readme, 'a') { |f|
     f.puts '<hr/>'
-    render(:free, data.free.categories, f)
-    render(:demo, data.demo.categories, f)
-    render(:paid, data.paid.categories, f)
+    render_toc(f)
+    render_categories(:free, data.free, f)
+    render_categories(:demo, data.demo, f)
+    render_categories(:paid, data.paid, f)
 }
 
 puts 'README file was generated';
